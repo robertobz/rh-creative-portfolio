b645e6e52c4ca5ef55ceb4c1d5519ad2c669d41b
Roberto H <125249665+robertobz@users.noreply.github.com>
2023-02-24 17:05:29 -0600

    Changed Default text in config and updated logo

--- src/gatsby-plugin-theme-ui/index.ts
+++ src/gatsby-plugin-theme-ui/index.ts
@@ -3,7 +3,7 @@ import originalTheme from "@lekoarts/gatsby-theme-jodie/src/gatsby-plugin-theme-
 
 const theme = merge(originalTheme, {
   fonts: {
-    body: `"Work Sans", -apple-system, BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"`,
+    body: `"Mark Pro", -apple-system, BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"`,
   },
 })
 
