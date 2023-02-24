7be290636e20e81685806f91174d855d14579a45
Roberto H <125249665+robertobz@users.noreply.github.com>
2023-02-24 17:08:11 -0600

    correct logo image url

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 const Logo = () => (
-<img src="https://github.com/robertobz/rh-creative-portfolio/blob/master/static/rh-creative-logo-colored-transparent.png"></img>
+<img src="https://raw.githubusercontent.com/robertobz/rh-creative-portfolio/master/static/rh-creative-logo-colored-transparent.png"></img>
 )
 
 export default Logo
